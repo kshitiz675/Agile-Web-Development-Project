7b1c171a0937dac279d21be63c3ac6f45fd51bda
TheRyuBlitz <81898441+TheRyuBlitz@users.noreply.github.com>
2021-04-18 19:21:01 +0800

    flask_login_step2

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('Theme and Purpose.html', title='Home')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('Login.html', title='Login', form=form)
+
+@app.route('/registration')
+def registration():
+    return render_template('Registration.html', title='Registration')
