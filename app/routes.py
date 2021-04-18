3ed0fcfc813de5f98f70f263a3f52b67938ec998
TheRyuBlitz <81898441+TheRyuBlitz@users.noreply.github.com>
2021-04-18 09:39:39 +0800

    flask_restart

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,8 @@
 from flask import render_template, url_for
 from app import app
-#from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('Theme and Purpose.html', title='Home')
-"""
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('Login.html', title='Sign In', form=form)
-"""
+
