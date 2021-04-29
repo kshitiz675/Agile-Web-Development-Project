b709e9c788239a45c22e314f2ed3073c76a2f5bb
TheRyuBlitz <81898441+TheRyuBlitz@users.noreply.github.com>
2021-04-29 09:12:56 +0800

    Update LoginForm & RegistrationForm

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
 
 with app.app_context():
     upgrade()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template, url_for, redirect
-from app import app
-#from app.forms import LoginForm
+from flask import render_template, url_for, redirect, flash
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+#from flask_login import login_required
+#Place @login_required for pages that require users to be signed in
 
 @app.route('/')
 @app.route('/index')
@@ -18,11 +22,32 @@ def assessment():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-   form = LoginForm()
-   if form.validate_on_submit():
-       return redirect('/index')
-   return render_template('Login.html', title='Login', form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect('/index')
+    return render_template('Login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/registration')
 def registration():
-    return render_template('Registration.html', title='Registration')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('Registration.html', title='Registration', form=form)
