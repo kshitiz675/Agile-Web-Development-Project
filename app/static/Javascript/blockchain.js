57e0c1c1c7e0dd493c8a7976436e637bdd4bf51b
kshitiz675 <kshitiz@live.ca>
2021-04-30 21:09:17 +0800

    js update

--- app/static/Javascript/blockchain.js
+++ app/static/Javascript/blockchain.js
@@ -1,10 +1,20 @@
 const progressBarFull = document.querySelector('#progressBarFull');
 const progressBar = document.querySelector('#progressBar');
+const video = document.querySelector('#video');
+const videoCaption = document.querySelector('.video-info')
 const MAX_PAGES = 5
-var page = 0;
+var page = 0; 
+var nextClickCount = 0;
+
 
 function progress() {
     page++;
-    // progressBarFull.style.width = `${(page/MAX_PAGES) * 100}%`;
-    progressBar.style.display = none;
+    nextClickCount++;
+    progressBarFull.style.width = `${(page / MAX_PAGES) * 100}%`;
+
+    video.remove();
+    videoCaption.remove();
+
+    
+
 }
