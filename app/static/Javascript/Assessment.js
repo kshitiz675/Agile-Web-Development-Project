e50d3d155bdf27850cf3cab0437279d10c953742
Will <will@hatchet.com.au>
2021-05-07 18:29:51 +0800

    Assessment page fix

--- app/static/Javascript/Assessment.js
+++ app/static/Javascript/Assessment.js
@@ -1,10 +1,14 @@
-let currentQuestionId = 1;
+
 const questionBlocks = document.getElementsByClassName('question');
+let currentQuestionId = getInitialQuestionId(questionBlocks[0].id);
+
 const originalQuestion = document.getElementById(`question-${currentQuestionId}`);
 originalQuestion.classList.remove('question-hidden');
 
 
 function nextQuestion() {
+
+    //Extract value of current question
     const currentQuestion = document.getElementById(`question-${currentQuestionId}`);
     currentQuestionId++;
     //If no next question do something
@@ -13,8 +17,9 @@ function nextQuestion() {
     currentQuestion.classList.add('question-hidden');
     nextQuestion.classList.remove('question-hidden');
 
+}
 
-
-    
+function getInitialQuestionId(questionId) {
+    return questionId.split('-')[1];
 
 }
