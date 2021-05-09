94058ff59da7199083b8a22e689456995a07ca86
Will <wknight801@gmail.com>
2021-05-09 10:28:44 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, request, jsonify, session
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user
-from app.models import User, Quiz, Topic
+from app.models import User, Quiz, Topic, Question, Answer, UserResult, QuizResult
 from flask_login import login_required
 #Place @login_required for pages that require users to be signed in
 
@@ -46,24 +46,62 @@ def lesson(id):
 def assessmentHome():
     return "Assessment Home Page"
     
-@app.route('/assessment/<int:quizId>')
+@app.route('/assessment/<int:quizId>', methods=['GET', 'POST'])
+@login_required
 def assessment(quizId):
     # There must be a variable that defines which topic this quiz will cover
-    quiz = Quiz.query.filter_by(id=quizId).first()
-    if quiz == None: return 'Not Found'
-    return render_template('Assesment.html', title='Assessment', quiz=quiz)
+    if request.method == 'GET':
+        quiz = Quiz.query.filter_by(id=quizId).first()
+        if quiz == None: return 'Not Found'
+        return render_template('Assesment.html', title='Assessment', quiz=quiz)
+    if request.method == 'POST':
+        data = request.get_json()
+        answers = data['answers']
+        numQuestions = len(answers)
+        numCorrect = 0
+        for answer in answers:
+            questionId = answer['questionId']
+            answerId = answer['answerId']
+            questionObj = Question.query.filter_by(id=questionId).first()
+            if questionObj == None:
+                return "Invalid"
+            answerObj = Answer.query.filter_by(id=answerId).first()
+            if answerObj == None or answerObj.questionId != questionObj.id:
+                return "Invalid"
+            if answerObj.correctAnswer: numCorrect += 1
+        userResults = UserResult.query.filter_by(userid = session['user_id']).first()
+        if userResults == None: 
+            return "Invalid User"
+        #TODO check if quiz has already been done
+        oldQuizResult = QuizResult.query.filter_by(userresultid=userResults.id).first()
+        if oldQuizResult != None:
+            return "Quiz already completed"
+        quizResult = QuizResult(quizid=quizId, userresultid=userResults.id, score=numCorrect)
+        db.session.add(quizResult)
+        db.session.commit()
+        userResults = UserResult.query.filter_by(userid=session['user_id']).first()
+        print("Success")
+        return "Success"
+
+        
+        
+
+
+
 
 
 #Login/Registration
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated: return redirect(url_for('index'))
+    if current_user.is_authenticated: 
+        session['user_id'] = current_user.id
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username = form.username.data).first()
-        if user == None or not user.check_password(form.password.data): 
-            return redirect(url_for('login'))
+        if user == None or not user.check_password(form.password.data): return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        session['user_id'] = user.id
         return redirect(url_for('index'))
     return render_template('Login.html', title='Login', form=form)
 
@@ -82,6 +120,12 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
+        #Todo check if refreshing works
+        userResults = UserResult(userid = User.query.filter_by(username=form.username.data).first().id)
+        db.session.add(userResults)
+        db.session.commit()
         return redirect(url_for('login'))
     return render_template('Registration.html', title='Registration', form=form)
+
+
 
